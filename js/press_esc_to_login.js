f490c4bddb99f0dbe28d5ae1b7cca935d23cf664
sam_undefined <sam.undefined.dev@gmail.com>
2018-11-30 08:50:56 -0600

    Add drupal 8 support to project

--- js/press_esc_to_login.js
+++ js/press_esc_to_login.js
@@ -1,7 +1,7 @@
 document.onkeydown = function(evt) {
   evt = evt || window.event;
 
-  var loginPath = Drupal.settings.pressEscToLogin.loginPath;
+  var loginPath = drupalSettings.pressEscToLogin.loginPath;
 
   if (evt.key === "Escape" && loginPath) {
     location.href = loginPath;
